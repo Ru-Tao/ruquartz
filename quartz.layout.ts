e6149454d0a1bb5fd48ae2163ed36be85f82933c
rutao <necoarc@tutanota.com>
2024-02-24 16:41:15 +0000

    Quartz sync: Feb 24, 2024, 4:41 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,7 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      Quartz: "https://github.com/jackyzha0/quartz",
     },
   }),
 }
@@ -20,6 +19,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
+    Component.MobileOnly(Component.Explorer()),
   ],
   left: [
     Component.PageTitle(),
